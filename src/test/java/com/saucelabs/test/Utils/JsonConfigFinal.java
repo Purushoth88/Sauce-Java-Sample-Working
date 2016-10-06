30b1aae2c728f0fbc52ff741ddaae700fe32eada
Purushoth88 <purush_it2007@yahoo.co.in>
2016-10-06 15:21:50 +0530

    Update JsonConfigFinal.java

--- src/test/java/com/saucelabs/test/Utils/JsonConfigFinal.java
+++ src/test/java/com/saucelabs/test/Utils/JsonConfigFinal.java
@@ -224,9 +224,11 @@ public class JsonConfigFinal {
 					+ fileName + "_" + new Random().nextInt(50046846) + ".xlsx";
 			System.out.println("file File: " + file);
 			FileOutputStream out = new FileOutputStream(generatedResultPath + ResultfileToImport, true);
-			FileOutputStream out = new FileOutputStream(System.getProperty("user.home") + "/OutputFolder/Results" + ResultfileToImport, true);
+			FileOutputStream out1 = new FileOutputStream(System.getProperty("user.home") + "/OutputFolder/Results" + ResultfileToImport, true);
 			System.out.println("out File: " + out);
+			System.out.println("out1 File: " + out1);
 			System.out.println("out File: " + out.getFD());
+			System.out.println("out1 File: " + out1.getFD());
 			for (Entry<Integer, String> e : pageList.entrySet()) {
 				Integer key = e.getKey();
 				String value = e.getValue();
