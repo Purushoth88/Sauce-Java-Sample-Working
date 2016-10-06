81b2ac3363701b1efbe3b3f6cda794f269296997
Purushoth88 <purush_it2007@yahoo.co.in>
2016-10-06 15:41:09 +0530

    Update JsonConfigFinal.java

--- src/test/java/com/saucelabs/test/Utils/JsonConfigFinal.java
+++ src/test/java/com/saucelabs/test/Utils/JsonConfigFinal.java
@@ -220,7 +220,7 @@ public class JsonConfigFinal {
 			String ResultfileToImport = "Result_"
 				+ fileName + "_" + new Random().nextInt(50046846) + ".xlsx";
 			System.out.println("ResultfileToImport File: " + ResultfileToImport);
-			String file = System.getProperty("user.home") + "\OutputFolder\Results\\Result_"
+			String file = System.getProperty("user.home") + "\\OutputFolder\\Results\\Result_"
 					+ fileName + "_" + new Random().nextInt(50046846) + ".xlsx";
 			System.out.println("file File: " + file);
 			FileOutputStream out = new FileOutputStream(ResultfileToImport, true);
