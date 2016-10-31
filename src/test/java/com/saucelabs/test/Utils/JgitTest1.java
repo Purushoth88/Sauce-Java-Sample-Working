9555049b333742c7607b6f0729ee5a86e5999bc2
Purushoth88 <purush_it2007@yahoo.co.in>
2016-10-31 13:39:13 +0530

    Update JgitTest1.java

--- src/test/java/com/saucelabs/test/Utils/JgitTest1.java
+++ src/test/java/com/saucelabs/test/Utils/JgitTest1.java
@@ -52,7 +52,7 @@ public class JgitTest1 {
 
 	@Before
 	public static void pushFiles() throws IOException, GitAPIException {
-		localPath = System.getProperty("user.home", "/CloneRepo/TempLocation");
+		localPath = System.getProperty("user.dir");
 		System.out.println("localPath" + localPath);
 		remotePath = "https://github.com/Purushoth88/Sauce-Java-Sample-Working.git";
 		//localRepo = new FileRepository(localPath + "/.git");
