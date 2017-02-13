2a439380033a0bd2e50b97357d72a7e22fc7b29c
Purushoth88 <purush_it2007@yahoo.co.in>
2017-02-13 15:28:50 +0530

    Update SauceRunner.java

--- src/test/java/com/saucelabs/test/Utils/SauceRunner.java
+++ src/test/java/com/saucelabs/test/Utils/SauceRunner.java
@@ -33,7 +33,7 @@ public class SauceRunner implements SauceOnDemandSessionIdProvider {
 		caps.setCapability("name", "PremierQCScriptTestJU");
 		//String seleniumURI = buildSauceUri();
 		wd =  new RemoteWebDriver(
-                new URL("https://" + username+ ":" + accesskey + "@ondemand.saucelabs.com:443" +"/wd/hub"),
+                new URL("https://" + username+ ":" + accesskey + "@maki76254.miso.saucelabs.com:443" +"/wd/hub"),
                 caps);
 		wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 		sessionId = wd.getSessionId().toString();
